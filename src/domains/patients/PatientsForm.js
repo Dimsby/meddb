f0106fe11e9d6b468fe6d221c1e8d640e5ebd8aa
Dimsby <Dimsby@gmail.com>
2021-09-18 20:59:12 +0300

    react hooks form

--- src/domains/patients/PatientsForm.js
+++ src/domains/patients/PatientsForm.js
@@ -1,83 +1,33 @@
 import {
+    Button,
     Divider,
     FormControl,
     FormControlLabel,
     FormLabel,
+    Grid,
     InputAdornment,
     Radio,
     RadioGroup,
     TextField
 } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
 import {useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
+
+import { useForm, Controller } from "react-hook-form";
 import {AccountCircle} from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({}))
 
 export default function PatientsForm(props) {
+    const { control, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     const classes = useStyles();
 
-    const defaultValues = {
-        firstName: "",
-        lastName: "",
-        surName: ""
-    };
-
-    const [formValues, setFormValues] = useState(defaultValues);
-
-    const handleInputChange = (e) => {
-        const {name, value} = e.target;
-        setFormValues({
-            ...formValues,
-            [name]: value,
-        });
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(formValues);
-    };
-
     return (
-        <form onSubmit={handleSubmit}>
-            <Grid container alignItems="center" justify="center" direction="column">
-                <Grid item>
-                    <TextField
-                        id="firstName-input"
-                        name="firstName"
-                        label="Имя"
-                        type="text"
-                        value={formValues.firstName}
-                        onChange={handleInputChange}
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <AccountCircle/>
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
-                </Grid>
-                <Grid item>
-                    <TextField
-                        id="lastName-input"
-                        name="lastName"
-                        label="Фамилия"
-                        type="text"
-                        value={formValues.firstName}
-                        onChange={handleInputChange}
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <AccountCircle/>
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
-                </Grid>
-            </Grid>
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+
             <Divider/>
             <Button color="primary" type="submit">
                 Сохранить
