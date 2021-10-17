519c3f7252dd3f0002fdd87871af6b3c12d32b28
Dimsby <Dimsby@gmail.com>
2021-10-17 19:17:43 +0300

    minor

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,7 @@ const resolvePath = p => path.resolve(__dirname, p)
 module.exports = {
     webpack: {
         configure: {
-            target: 'electron-renderer'
+            target: 'web'
         },
         alias: {
             '@components': resolvePath('./src/components'),
--- src/domains/patients/PatientsForm.js
+++ src/domains/patients/PatientsForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm, FormProvider } from "react-hook-form";
+import {useForm, FormProvider} from "react-hook-form";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 
@@ -8,41 +8,45 @@ import FormDatepicker from "@components/controls/datepicker";
 
 export default function PatientsForm(props) {
     const methods = useForm();
-    const { handleSubmit } = methods;
+    const {handleSubmit} = methods;
 
     const onSubmit = (data) => {
         return props.formSubmitHandler(data);
     };
 
     return (
-                <FormProvider {...methods}>
-                    <form>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <FormInput name="lastName" label="Фамилия" value="" margin="dense" fullWidth required />
-                                <FormInput name="firstName" label="Имя" value="" margin="dense" fullWidth required />
-                                <FormInput name="surName" label="Отчество" value="" margin="dense" fullWidth />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <FormDatepicker name="dateBirth" label="Дата рождения" value="" margin="dense" fullWidth required />
-                                <FormInput name="phone" label="Телефон" value="" type='number' margin="dense" fullWidth/>
-                            </Grid>
+        <FormProvider {...methods}>
+            <form>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <FormInput name="lastName" label="Фамилия" value="" margin="dense" fullWidth required/>
+                        <FormInput name="firstName" label="Имя" value="" margin="dense" fullWidth required/>
+                        <FormInput name="surName" label="Отчество" value="" margin="dense" fullWidth/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormDatepicker name="dateBirth" label="Дата рождения" value="" margin="dense" fullWidth
+                                        required/>
+                        <FormInput name="phone" label="Телефон" value="" type='number' margin="dense" fullWidth/>
+                    </Grid>
 
-                            <Grid item xs={6}>
-                                <FormInput name="adrReg" label="Адрес регистрации" value="" margin="dense" minRows={3} maxRows={6} multiline fullWidth />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <FormInput name="adrPhys" label="Адрес пребывания" value="" margin="dense" minRows={3} maxRows={6} multiline fullWidth />
-                            </Grid>
+                    <Grid item xs={6}>
+                        <FormInput name="adrReg" label="Адрес регистрации" value="" margin="dense" minRows={3}
+                                   maxRows={6} multiline fullWidth/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormInput name="adrPhys" label="Адрес пребывания" value="" margin="dense" minRows={3}
+                                   maxRows={6} multiline fullWidth/>
+                    </Grid>
 
-                            <Grid item xs={6}>
-                                <FormInput name="jobPlace" label="Место работы" value="" margin="dense" minRows={3} maxRows={6} multiline fullWidth />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <FormInput name="jobPos" label="Должность" value="" margin="dense" fullWidth />
-                            </Grid>
+                    <Grid item xs={6}>
+                        <FormInput name="jobPlace" label="Место работы" value="" margin="dense" minRows={3} maxRows={6}
+                                   multiline fullWidth/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormInput name="jobPos" label="Должность" value="" margin="dense" fullWidth/>
+                    </Grid>
 
-                            {/*
+                    {/*
                             CREATE TABLE "Patients" (
                             "id"	INTEGER,
                             "lastName"	TEXT NOT NULL,
@@ -56,18 +60,18 @@ export default function PatientsForm(props) {
                             "jobPos"	TEXT,
                             PRIMARY KEY("id" AUTOINCREMENT)
                             ) */}
-                        </Grid>
+                </Grid>
 
-                        <Button
-                            sx={{marginTop: 2, float: 'right'}}
-                            variant="contained"
-                            color="primary"
-                            onClick={handleSubmit(onSubmit)}
-                        >
-                            Сохранить
-                        </Button>
-                    </form>
+                <Button
+                    sx={{marginTop: 2, float: 'right'}}
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit(onSubmit)}
+                >
+                    Сохранить
+                </Button>
+            </form>
 
-                </FormProvider>
+        </FormProvider>
     );
 }
